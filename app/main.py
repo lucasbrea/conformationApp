8cf05ecb8c843ac1b5af4d7748207830a654a07e
move37-vpn13 <move37-vpn13@move37.local>
2026-02-04 12:58:36 -0500

    Added static interface

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.routes.infer import router as infer_router
 from app.routes.runs import router as runs_router
+from fastapi.staticfiles import StaticFiles
+
+app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
 
 app = FastAPI()
 app.mount("/data", StaticFiles(directory="/data"), name="data")
